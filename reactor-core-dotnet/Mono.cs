56b89b0e5104c45904f670bebfb78dea3bf18ca8
akarnokd <akarnokd@gmail.com>
2016-06-03 16:26:24 +0200

    Small adjustments

--- reactor-core-dotnet/Mono.cs
+++ reactor-core-dotnet/Mono.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Reactor.Core
 {
-    public static class Flux
+    public static class Mono
     {
     }
 }
